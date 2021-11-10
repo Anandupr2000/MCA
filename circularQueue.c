6bf38ef65a812b2ff9da8220b0f629bccc397cab
Anandu P R <ananduprajesh@gmail.com>
2021-11-10 19:54:17 +0530

    circular queue read fn added

---
+++ circularQueue.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int *array;
+void read(int* a,int count){
+    for(int i=0;i<count;i++) scanf("%d",&a[i]);
+    }
+// void dequeue(int *a,){}
+void enqueue(){}
+void main(){
+    int *arr1, *arr2, i,count1, count2;
+    printf("Enter no of elements in queue : ");
+    scanf("%d",&count1);
+    read(arr1,count1);
+}
