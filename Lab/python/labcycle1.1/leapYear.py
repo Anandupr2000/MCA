83e831e8ddabdd78dbd5432a99064d856605942e
Anandupr2000 <80123491+Anandupr2000@users.noreply.github.com>
2021-12-19 17:55:25 +0530

    leapyear Prgm fixed

--- Lab/python/labcycle1.1/leapYear.py
+++ Lab/python/labcycle1.1/leapYear.py
@@ -3,7 +3,9 @@ eYear = int(input("Enter end year : "))
 print(f"Leap years between {sYear} and {eYear} are : ")
 
 while sYear<=eYear:
-	if sYear%4==0 and (sYear%400==0 or sYear%100!=0):
+	if sYear%4==0 and sYear%100!=0:
 		print(f"{sYear}")
 	sYear = sYear+1
+
+
 
